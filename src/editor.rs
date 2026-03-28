3b84f9ae7e0f7502b16f863e29e75d1e7fa18d45
“miel” <nobrujpp@gmail.com>
2026-03-28 15:22:27 -0300

    being fodinha with the buffer

--- src/editor.rs
+++ src/editor.rs
@@ -15,7 +15,17 @@ use ratatui::{prelude::*, widgets::*};
 use ratatui::{DefaultTerminal, Frame};
 use std::fs::File;
 
+struct Buffer {
+    text_file: String,
+
+}
+
 pub fn start() -> std::io::Result<()> {
+    let mut string_file: String = std::fs::read_to_string("cudecachorro.txt").expect("erro");
+    let mut buffer = Buffer { 
+         text_file: string_file
+    };
+
     loop {
         let backend = CrosstermBackend::new(stderr());
         let mut terminal = Terminal::new(backend)?;
@@ -24,14 +34,16 @@ pub fn start() -> std::io::Result<()> {
         stdout().execute(EnterAlternateScreen)?;
 
         terminal.clear()?;
-        terminal.draw(render)?;
+        terminal.draw(|frame| render(&buffer, frame))?;
         if let Event::Key(key) = event::read()? { 
            match key.code {
                KeyCode::Char('q') => {
+                    std::fs::write("cudecachorro.txt", buffer.text_file).unwrap();
                     stdout().execute(LeaveAlternateScreen)?;
                     disable_raw_mode()?;
                     break;
                 },
+                KeyCode::Char(c) => buffer.text_file.push(c),
                 _ => {}
             }
         }
@@ -39,15 +51,14 @@ pub fn start() -> std::io::Result<()> {
    Ok(()) 
 }
 
-fn render(frame: &mut Frame) {
+fn render(arg: &Buffer, frame: &mut Frame) {
     let vertical = Layout::vertical([Constraint::Length(1), Constraint::Fill(1)]).spacing(1);
     let horizontal = Layout::horizontal([Constraint::Percentage(50); 2]).spacing(1);
     let [top, main] = frame.area().layout(&vertical);
     let [first, second] = main.layout(&horizontal);
 
-    let read_file: String = std::fs::read_to_string("cudecachorro.txt").expect("erro");
-    
-    let paragraph = Paragraph::new(read_file)
+
+    let paragraph = Paragraph::new(arg.text_file.clone())
         .style(Color::White)
         .alignment(Alignment::Left);
 
